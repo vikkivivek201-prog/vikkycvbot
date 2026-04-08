c8e02f6cb20d3cf335e3665699f0be4b5597edf9
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-08 22:36:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -273,10 +273,11 @@ if filename.endswith(".vcf") and state.get("mode") == "vcf_to_txt":
         os.remove(path)
 
 # 🔥 NAME MAANGO
+
 if "txt_name" not in state:
     state["step"] = "ask_name"
     update.message.reply_text("Enter output TXT file name:")
-    return
+        return
 
 # =========================
 # 🔄 MERGE VCF (NO SPAM)
