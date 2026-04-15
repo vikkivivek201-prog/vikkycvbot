ccb154cef46e9acfcdd10e8e94d854c71b416d80
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-15 16:54:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,30 +23,40 @@ def home():
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "5328734113"))
 
-main_menu = [
-    [
-        KeyboardButton(
-            text="Text to VCF",
-            icon_custom_emoji_id="5433653135799228968"
-        ),
-        KeyboardButton(
-            text="VCF to Text",
-            icon_custom_emoji_id="5431736674147114227"
-        )
-    ],
-    [
-        KeyboardButton(text="Manual VCF"),
-        KeyboardButton(text="Manual Text")
-    ],
-    [
-        KeyboardButton(text="Merge VCF"),
-        KeyboardButton(text="Split Text")
-    ],
-    [
-        KeyboardButton(text="VCF Editer"),
-        KeyboardButton(text="My Subscription")
-    ]
-]
+
+def main_menu():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    
+    # Row 1
+    kb.row(
+        types.KeyboardButton("📁 Text to VCF", style="primary", icon_custom_emoji_id="5433653135799228968"),
+        types.KeyboardButton("📄 VCF to Text", style="primary", icon_custom_emoji_id="5431736674147114227")
+    )
+    
+    # Row 2
+    kb.row(
+        types.KeyboardButton("📄 Manual VCF", style="success", icon_custom_emoji_id="6266995104687330978"),
+        types.KeyboardButton("📁 Manual Text", style="primary", icon_custom_emoji_id="5334673106202010226")
+    )
+    
+    # Row 3
+    kb.row(
+        types.KeyboardButton("🔄 Merge VCF", style="primary", icon_custom_emoji_id="5264727218734524899"),
+        types.KeyboardButton("✂️ Split Text", style="primary", icon_custom_emoji_id="5258477770735885832")
+    )
+    
+    # Row 4
+    kb.row(
+        types.KeyboardButton("✍️ VCF Editer", style="primary", icon_custom_emoji_id="5237808360882977239"),
+        types.KeyboardButton("🔍Get VCF details", style="danger", icon_custom_emoji_id="5893382531037794941")
+    )
+    
+    # Row 5
+    kb.row(
+        types.KeyboardButton("💳 Premium", style="success", icon_custom_emoji_id="5902432207519093015")
+    )
+    
+    return kb
 
 
 user_state = {}
