78e700e3e02e1b59d71433a5321757da76d931d6
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-16 21:41:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def main_menu():
     # Row 1
     kb.row(
         types.KeyboardButton("Text to VCF", style="primary", icon_custom_emoji_id="5433653135799228968"),
-        types.KeyboardButton("VCF to Text", style="primary", icon_custom_emoji_id="5431736674147114227")
+        types.KeyboardButton("VCF to Text", style="secondary", icon_custom_emoji_id="5431736674147114227")
     )
     
     # Row 2
