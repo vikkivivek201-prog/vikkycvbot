5a0328d791e349582bd77cd2cfc1e80d61d21d28
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-21 02:54:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ def main_menu():
     
     # Row 5
     kb.row(
-        types.KeyboardButton("VCF EditOr", style="primary", icon_custom_emoji_id="5334673106202010226"),
+        types.KeyboardButton("VCF Editor", style="primary", icon_custom_emoji_id="5334673106202010226"),
         types.KeyboardButton("Get VCF details", style="primary", icon_custom_emoji_id="5188217332748527444")
     )
     
